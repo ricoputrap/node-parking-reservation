2de9a4bb41c0408e833f803862ffd700e562fc8b
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-10-02 21:49:04 +0700

    feat: prepare routing for all use cases or features

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import { IncomingMessage, ServerResponse, createServer } from 'http';
 import authRoute from './features/auth/auth.route';
 import log from './utils/logger';
 import { PORT } from '../config/constants';
+import spotsRoute from './features/spots/spots.route';
+import reservationsRoute from './features/reservations/reservations.route';
+import paymentsRoute from './features/payments/payments.route';
+
+const notFoundHandler = (req: IncomingMessage, res: ServerResponse) => {
+  res.statusCode = 404;
+  res.write(JSON.stringify({ error: 'Route not found' }));
+  res.end();
+}
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   res.setHeader('Content-Type', 'application/json');
@@ -12,9 +21,16 @@ const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   console.log(loggedMessage);
   log(loggedMessage);
 
-
-  if (req.url?.startsWith("/api/auth")) authRoute(req, res);
-  else res.end();
+  if (req.url?.startsWith("/api/auth"))
+    authRoute(req, res);
+  else if (req.url?.startsWith("/api/spots"))
+    spotsRoute(req, res);
+  else if (req.url?.startsWith("/api/reservations"))
+    reservationsRoute(req, res);
+  else if (req.url?.startsWith("/api/payments"))
+    paymentsRoute(req, res);
+  else
+    notFoundHandler(req, res);
 });
 
 server.listen(PORT, () => {
